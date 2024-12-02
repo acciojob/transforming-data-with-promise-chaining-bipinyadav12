d2962f165470baeccb0237d2c9f61af22606e3aa
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-12-02 13:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-//your JS code here. If required.
+const button=document.getElementById("btn");
+button.addEventListener('click',function (input) {
+const inputElement=document.getElementById("ip");
+const output=document.getElementById("output");
+output.textContent='';
+input = parseFloat(inputElement.value);
+	
+	const firstPromise=new Promise ((resolve)=>{
+		setTimeout(()=>{
+			resolve(input);
+		},2000)
+	})
+	firstPromise.then((input)=>{
+		output.textContent=`Result:${input}`;
+		return new Promise((reslove)=>{
+			setTimeout(()=> reslove(input*2), 1000);
+		});
+	})
+	.then((input)=>{
+		output.textContent=`Result:${input}`;
+		return new Promise((reslove)=>{
+			setTimeout(()=>{
+				reslove(input-3)
+			},1000)
+		})
+	}).then((input)=>{
+		showResult(input)
+		return new Promise((reslove)=>{
+			setTimeout(()=>{
+				reslove(input/2)
+			},1000)
+		})
+	}).then((input)=>{
+		showResult(input)
+		return new Promise((reslove)=>{
+			setTimeout(()=>{
+				reslove(input+10)
+			},1000)
+		})
+	}).then((input)=>{
+		output.textContent=`Final Result:${input}`
+	})
+	
+});
+
+function showResult(input) {
+	output.textContent=`Result:${input}`
+} 
